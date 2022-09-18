220138b071fd7f634b41bd4e40abf2b8134335b6
julu1994 <mrjewel837@gmail.com>
2022-09-18 04:32:47 +0200

    chaging feedback states by clicking

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -4,6 +4,32 @@ import { ImHappy, ImAngry, ImWink, ImSad, ImNeutral } from "react-icons/im";
 import { GiHamburgerMenu } from "react-icons/gi";
 
 const Home = () => {
+    const [terrible, setTerrible] = React.useState(0);
+    const [bad, setBad] = React.useState(0);
+    const [meh, setMeh] = React.useState(0);
+    const [good, setGood] = React.useState(0);
+    const [excellent, setExcellent] = React.useState(0);
+
+    const terribleHandler = () => {
+        setTerrible((value) => value + 1);
+    };
+    const badHandler = () => {
+        setBad((value) => value + 1);
+    };
+    const mehHandler = () => {
+        setMeh((value) => value + 1);
+    };
+    const goodHandler = () => {
+        setGood((value) => value + 1);
+    };
+    const excellentHandler = () => {
+        setExcellent((value) => value + 1);
+    };
+    console.log(terrible, "terrible");
+    console.log(bad, "bad");
+    console.log(meh, "meh");
+    console.log(good, "good");
+    console.log(excellent, "excellent");
     return (
         <div className="home">
             <div className="home-nav">
@@ -14,23 +40,23 @@ const Home = () => {
                 />
             </div>
             <div className="home-emoji">
-                <button className="home-btn">
+                <button className="home-btn" onClick={terribleHandler}>
                     <ImAngry size={90} color={"#FF0000"} />
                     <p>Terrible</p>
                 </button>
-                <button className="home-btn">
+                <button className="home-btn" onClick={badHandler}>
                     <ImSad size={90} color={"#FF6347"} />
                     <p>Bad</p>
                 </button>
-                <button className="home-btn">
+                <button className="home-btn" onClick={mehHandler}>
                     <ImNeutral size={90} color={"#FFD700"} />
                     <p>Meh</p>
                 </button>
-                <button className="home-btn">
+                <button className="home-btn" onClick={goodHandler}>
                     <ImHappy size={90} color={"#00FF00"} />
                     <p>Good</p>
                 </button>
-                <button className="home-btn">
+                <button className="home-btn" onClick={excellentHandler}>
                     <ImWink size={90} color={"#32CD32"} />
                     <p>Excellent</p>
                 </button>
