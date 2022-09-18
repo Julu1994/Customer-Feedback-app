1bafb0c27b7ad3054cd30617f11d7a78657b4638
julu1994 <mrjewel837@gmail.com>
2022-09-18 03:37:03 +0200

    added animation to the emojies

--- src/Pages/home.js
+++ src/Pages/home.js
@@ -1,22 +1,25 @@
 import "./home.scss";
 import React from "react";
-import { ImHappy, ImAngry, ImWink, ImSad } from "react-icons/im";
+import { ImHappy, ImAngry, ImWink, ImSad, ImNeutral } from "react-icons/im";
 
 const Home = () => {
     return (
         <div className="home">
             <div className="home-emoji">
                 <button className="home-btn">
-                    <ImHappy size={90} color={"red"} />
+                    <ImAngry size={90} color={"#FF0000"} />
                 </button>
                 <button className="home-btn">
-                    <ImAngry size={90} color={"red"} />
+                    <ImSad size={90} color={"#FF6347"} />
                 </button>
                 <button className="home-btn">
-                    <ImWink size={90} color={"red"} />
+                    <ImNeutral size={90} color={"#FFD700"} />
                 </button>
                 <button className="home-btn">
-                    <ImSad size={90} color={"red"} />
+                    <ImHappy size={90} color={"#00FF00"} />
+                </button>
+                <button className="home-btn">
+                    <ImWink size={90} color={"#32CD32"} />
                 </button>
             </div>
         </div>
